68d8649a8507dd0265dcc74a96fa852895c40b7d
Daniil Aleksandrov <riosha3@gmail.com>
2021-10-26 10:22:43 +0300

    убрал ошибку

--- bot.py
+++ bot.py
@@ -32,7 +32,7 @@ class Bot:
             }
             for event in self.longpoll.listen():
                 if event.type in event_data.keys():
-                    event_data[event.type](events)
+                    event_data[event.type](event)
         except Exception:
             self.__logger()
 
